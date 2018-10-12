902053158a3d1b81c6ae8b2346cadfdf1aa6139b
shark1615 <rabbit212121@gmail.com>
2018-10-13 06:22:28 +0800

    :)

---
+++ hw01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw01
+//
+//  Created by Apple on 2018/10/9.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ hw01Tests/hw01Tests.swift
@@ -0,0 +1,34 @@
+//
+//  hw01Tests.swift
+//  hw01Tests
+//
+//  Created by Apple on 2018/10/9.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+import XCTest
+@testable import hw01
+
+class hw01Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
